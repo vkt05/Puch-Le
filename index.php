37f0c816a3d23ac34e48d22ace2200f7a83508f1
Vishal Tiwari <vishaltiwari909080@gmail.com>
2024-09-06 16:35:43 +0530

    folder structured

--- index.php
+++ index.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PuchLe-This is a forum were you can ask question on desfined topics. </title>
+    <link rel="stylesheet" href="./public/stylesheets/index.css">
+    <link rel="stylesheet" href="./public/stylesheets/navbar.css">
+    
 </head>
 <body>
-    
+    <?php
+    include './partials/_navbar.php';
+    ?>
 </body>
 </html>
---
+++ partials/_navbar.php
@@ -0,0 +1,15 @@
+
+<header>
+    <nav>
+        <div id="logo">
+            <h2>PuchLe</h2>
+        </div>
+        <div id="nav-items">
+            <ul>
+                <li>Categories</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+    </nav>
+</header>
